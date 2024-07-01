cff38a88af2dd1c0a6b4bc59b3d2d48c3122a0bc
vignesh2k2 <vigneshm2k24@gmail.com>
2024-07-02 02:00:50 +0530

    scroll top btn added

--- DNclone.js
+++ DNclone.js
@@ -276,3 +276,27 @@ const observer4 = new IntersectionObserver((entries) =>{
 
 const downElements = document.querySelectorAll('.down');
 downElements.forEach((el) => observer4.observe(el));
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - 
+    document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+    if(pos>100){
+        scrollProgress.style.display = "grid";
+    }
+    else{
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click",() => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient 
+    (#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+    // console.log(scrollValue); 
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
